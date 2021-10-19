4ebe5e2973652ac92e5ec0a99b9f8bfa31c40f02
quanjindeng <54259993+quanjindeng@users.noreply.github.com>
2021-10-20 01:22:11 +0800

    Update diy-part.sh

--- build/openwrt_amlogic/diy-part.sh
+++ build/openwrt_amlogic/diy-part.sh
@@ -33,7 +33,7 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                          
 # 设置打包固件的机型，内核组合（可用内核是时时变化的,过老的内核就删除的，所以要选择什么内核请看说明）
 cat >$GITHUB_WORKSPACE/amlogic_openwrt <<-EOF
 amlogic_model=s905d
-amlogic_kernel=5.4.154
+amlogic_kernel=5.4.150
 rootfs_size=1024
 EOF
 
